66b28c025d3a85b1a7f89b5c5d7a6b5666d3790a
Cat-Hanbit <gasida.seo@gmail.com>
2021-06-20 11:07:20 +0900

    k3s flannel config change

--- K3S/master_node.sh
+++ K3S/master_node.sh
@@ -3,7 +3,7 @@ MASTER_IP=192.168.200.10
 HOSTNAME=$(cat /etc/hostname)
 
 # install k3s
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--advertise-address $MASTER_IP --disable-cloud-controller --node-name $HOSTNAME --docker" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--advertise-address $MASTER_IP --flannel-backend=vxlan --flannel-iface=enp0s8 --disable-cloud-controller --node-name $HOSTNAME --docker" sh -s -
 
 # source bash-completion for kubectl kubeadm
 source <(kubectl completion bash)
--- K3S/worker_nodes.sh
+++ K3S/worker_nodes.sh
@@ -6,4 +6,4 @@ HOSTNAME=$(cat /etc/hostname)
 apt-get install sshpass -y
 sshpass -p "qwe123" scp -o StrictHostKeyChecking=no root@$MASTER_IP:/var/lib/rancher/k3s/server/node-token /root
 NODE_TOKEN=$(cat /root/node-token)
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_EXEC="--node-name $HOSTNAME --docker" sh -s -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$NODE_TOKEN INSTALL_K3S_EXEC="--node-name $HOSTNAME --flannel-iface=enp0s8 --docker" sh -s -
